98b7d2e04e69f93d01fbb10f2ff9b502e36083c7
Eli Lev <eli@starlev.com>
2024-08-22 21:33:21 +0300

    First upload

---
+++ src/components/JewelryOrdersTable/columns.js
@@ -0,0 +1,27 @@
+import { createColumnHelper } from "@tanstack/react-table";
+
+
+const columnHelper = createColumnHelper();
+
+export const columnDef = [
+  columnHelper.accessor("id", {
+    header: "Id",
+  }),
+  {
+    accessorKey:"first_name",
+    header: "First Name",
+  },
+  {
+    accessorKey: "last_name",
+    header: "Last Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    
+  },
+];
